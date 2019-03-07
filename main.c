72353b6c01d49b73494e4635a550bb23c3aa1f47
Joseph Ryan <46049971+Joehac02@users.noreply.github.com>
2019-03-07 10:28:36 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+
+task main()
+{
+while(true)
+//if you touch the arm button
+//wait 10
+//if the light senosor senses a higher light level
+	//wait 5
+	//if touch the all clear button
+	//end program
+	//else
+	//light up LEDs
+	//if keyLightSensor gets a low value (lazer)
+	//then end program
+
+
+}
